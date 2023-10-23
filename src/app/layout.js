2a3e37987dbd7a123628de4143d90725b8601f08
sathwik_karne <karnesathwik@gmail.com>
2023-10-23 12:50:26 +0530

    initial layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,28 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+import Navbar from "@/components/navbar/Navbar";
+import Footer from "@/components/footer/Footer";
 
 export const metadata = {
-  title: 'Blog App',
-  description: 'The best blog app!',
-}
+  title: "Blog App",
+  description: "The best blog app!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="container">
+          <div className="wrapper">
+            {/* navbar and footer are in all pages. Hence it is placed in layout */}
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/CardList/CardList";
+import Menu from "@/components/Menu/Menu";
+
+
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+  <div className={styles.container}>
+    <Featured/>
+    <CategoryList/>
+    <div className={styles.content}>
+      <CardList/>
+      <Menu/>
+    </div>
+      
+  </div>
+  );
 }
